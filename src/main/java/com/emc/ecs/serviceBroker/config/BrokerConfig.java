b6ee444f5c874c21eed598bfb45bce66c3d0b7f8
Aaron Spiegel <spiegela@gmail.com>
2015-12-02 16:28:32 -0600

    Set broker to version 2.7

--- src/main/java/com/emc/ecs/serviceBroker/config/BrokerConfig.java
+++ src/main/java/com/emc/ecs/serviceBroker/config/BrokerConfig.java
@@ -2,6 +2,7 @@ package com.emc.ecs.serviceBroker.config;
 
 import java.net.URL;
 
+import org.cloudfoundry.community.servicebroker.model.BrokerApiVersion;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,13 +17,18 @@ public class BrokerConfig {
 	@Bean
 	public Connection ecsConnection() {
 		URL certificate = getClass().getClassLoader().getResource("localhost.pem");
-		return new Connection("https://8.34.215.78:4443", "root", "ChangeMe", certificate);
+		return new Connection("https://104.197.239.202:4443", "root", "ChangeMe", certificate);
+	}
+	
+	@Bean
+	public BrokerApiVersion brokerApiVersion() {
+		return new BrokerApiVersion("2.7");
 	}
 	
 	@Bean
 	public EcsRepositoryCredentials getRepositoryCredentials() {
 		return new EcsRepositoryCredentials("ecs-cf-service-broker-repository", "ecs-cf-service-broker-repository",
-				"ns1", "urn:storageos:ReplicationGroupInfo:d4fc7068-1051-49ee-841f-1102e44c841e:global");
+				"ns1", "urn:storageos:ReplicationGroupInfo:f81a7335-cadf-48fb-8eda-4856b250e9de:global");
 	}
 
 }
