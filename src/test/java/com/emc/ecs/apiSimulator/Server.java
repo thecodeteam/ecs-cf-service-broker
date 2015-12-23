75b92d7b7594e98dc95deb29d6cf5abe8e6fd59d
Aaron Spiegel <spiegela@gmail.com>
2015-12-22 23:05:17 -0600

    Move ECS management API simulator to default port

--- src/test/java/com/emc/ecs/apiSimulator/Server.java
+++ src/test/java/com/emc/ecs/apiSimulator/Server.java
@@ -9,7 +9,7 @@ public class Server {
 		WireMockServer wireMockServer = new WireMockServer(
 				wireMockConfig()
 					.port(9020) 	   // use HTTP port for s3
-					.httpsPort(8443)); // use HTTPS port for ecs-management 
+					.httpsPort(4443)); // use HTTPS port for ecs-management 
 		wireMockServer.start();
 	}
 
--- src/test/java/com/emc/ecs/common/EcsActionTest.java
+++ src/test/java/com/emc/ecs/common/EcsActionTest.java
@@ -6,7 +6,7 @@ import com.emc.ecs.managementClient.Connection;
 
 public abstract class EcsActionTest {
 	protected URL certificate = getClass().getClassLoader().getResource("localhost.pem");
-	protected Connection connection = new Connection("https://127.0.0.1:8443", "root", "ChangeMe", certificate);
+	protected Connection connection = new Connection("https://127.0.0.1:4443", "root", "ChangeMe", certificate);
 	protected String namespace = "ns1";
 	protected String baseUrlHost = "localhost";
 	protected String replicationGroup = "urn:storageos:ReplicationGroupInfo:2ef0a92d-cf88-4933-90ba-90245aa031b1:global";
