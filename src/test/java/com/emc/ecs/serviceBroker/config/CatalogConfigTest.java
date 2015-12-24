81775c319e540c30b951a861f39ff124ae5a10b7
Aaron Spiegel <spiegela@gmail.com>
2015-12-23 19:32:34 -0600

    Load config file for app context

--- src/test/java/com/emc/ecs/serviceBroker/config/CatalogConfigTest.java
+++ src/test/java/com/emc/ecs/serviceBroker/config/CatalogConfigTest.java
@@ -13,12 +13,12 @@ import org.cloudfoundry.community.servicebroker.model.ServiceDefinition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = CatalogConfig.class, loader = AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes = Application.class, initializers = ConfigFileApplicationContextInitializer.class)
 public class CatalogConfigTest {
 
 	@Autowired
