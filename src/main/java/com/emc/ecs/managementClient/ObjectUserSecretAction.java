a6685d10a4cce26284ad53be946888ae36f8156c
Mohamed Ezzat <mohamed.ezzat@devfactory.com>
2016-05-05 19:41:41 +0200

    squid:S1192 - String literals should not be duplicated

--- src/main/java/com/emc/ecs/managementClient/ObjectUserSecretAction.java
+++ src/main/java/com/emc/ecs/managementClient/ObjectUserSecretAction.java
@@ -12,10 +12,13 @@ import com.emc.ecs.serviceBroker.EcsManagementClientException;
 
 public class ObjectUserSecretAction {
 
+	private static final String OBJECT = "object";
+	private static final String USER_SECRET_KEYS = "user-secret-keys";
+
 	public static UserSecretKey create(Connection connection, String id)
 			throws EcsManagementClientException {
-		UriBuilder uri = connection.getUriBuilder().segment("object",
-				"user-secret-keys", id);
+		UriBuilder uri = connection.getUriBuilder().segment(OBJECT,
+				USER_SECRET_KEYS, id);
 		Response response = connection.handleRemoteCall("post", uri,
 				new UserSecretKeyCreate());
 		return response.readEntity(UserSecretKey.class);
@@ -23,8 +26,8 @@ public class ObjectUserSecretAction {
 
 	public static UserSecretKey create(Connection connection, String id,
 			String key) throws EcsManagementClientException {
-		UriBuilder uri = connection.getUriBuilder().segment("object",
-				"user-secret-keys", id);
+		UriBuilder uri = connection.getUriBuilder().segment(OBJECT,
+				USER_SECRET_KEYS, id);
 		Response response = connection.handleRemoteCall("post", uri,
 				new UserSecretKeyCreate(key));
 		return response.readEntity(UserSecretKey.class);
@@ -32,8 +35,8 @@ public class ObjectUserSecretAction {
 
 	public static List<UserSecretKey> list(Connection connection, String id)
 			throws EcsManagementClientException {
-		UriBuilder uri = connection.getUriBuilder().segment("object",
-				"user-secret-keys", id);
+		UriBuilder uri = connection.getUriBuilder().segment(OBJECT,
+				USER_SECRET_KEYS, id);
 		Response response = connection.handleRemoteCall("get", uri, null);
 		return response.readEntity(UserSecretKeyList.class).asList();
 	}
