82f576c6f934c748530220e79567aa7c0a2a23a6
Aaron Spiegel <spiegela@gmail.com>
2016-05-17 17:48:15 -0500

    Include tests in test-suite and use for travis config
    This removes the requirement to start the simulator prior to test runs, making gradle/travis tests more reliable.  The simulator is left intact since it helps when running individual tests.

---
+++ src/test/java/com/emc/ecs/TestSuite.java
@@ -0,0 +1,61 @@
+package com.emc.ecs;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.emc.ecs.managementClient.BaseUrlActionTest;
+import com.emc.ecs.managementClient.BucketAclActionTest;
+import com.emc.ecs.managementClient.BucketActionTest;
+import com.emc.ecs.managementClient.BucketQuotaActionTest;
+import com.emc.ecs.managementClient.ConnectionTest;
+import com.emc.ecs.managementClient.NamespaceActionTest;
+import com.emc.ecs.managementClient.ObjectUserActionTest;
+import com.emc.ecs.managementClient.ObjectUserSecretActionTest;
+import com.emc.ecs.managementClient.ReplicationGroupActionTest;
+import com.emc.ecs.serviceBroker.EcsServiceTest;
+import com.emc.ecs.serviceBroker.config.CatalogConfigTest;
+import com.emc.ecs.serviceBroker.model.ServiceDefinitionProxyTest;
+import com.emc.ecs.serviceBroker.repository.ServiceInstanceBindingRepositoryTest;
+import com.emc.ecs.serviceBroker.repository.ServiceInstanceRepositoryTest;
+import com.emc.ecs.serviceBroker.service.EcsServiceInstanceServiceTest;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    BaseUrlActionTest.class,
+    BucketAclActionTest.class,
+    BucketActionTest.class,
+    BucketQuotaActionTest.class,
+    ConnectionTest.class,
+    NamespaceActionTest.class,
+    ObjectUserActionTest.class,
+    ObjectUserSecretActionTest.class,
+    ReplicationGroupActionTest.class,
+    EcsServiceTest.class,
+    CatalogConfigTest.class,
+    ServiceDefinitionProxyTest.class,
+    ServiceInstanceBindingRepositoryTest.class,
+    ServiceInstanceRepositoryTest.class,
+    EcsServiceInstanceServiceTest.class
+    })
+public class TestSuite {
+
+    @Rule
+    public static WireMockRule wireMockRule = new WireMockRule(
+	    WireMockConfiguration.wireMockConfig().port(9020).httpsPort(4443));
+
+    @BeforeClass
+    public static void setUp() {
+	wireMockRule.start();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+	wireMockRule.shutdownServer();
+    }
+}
