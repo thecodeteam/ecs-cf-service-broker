cd8c665b5f8a5963440cb99e28243132d2130bf2
Aaron Spiegel <spiegela@gmail.com>
2016-05-19 19:29:43 -0500

    Reformat whitespace

--- src/main/java/com/emc/ecs/serviceBroker/service/EcsServiceInstanceBindingService.java
+++ src/main/java/com/emc/ecs/serviceBroker/service/EcsServiceInstanceBindingService.java
@@ -27,73 +27,79 @@ import com.emc.ecs.serviceBroker.repository.ServiceInstanceBinding;
 import com.emc.ecs.serviceBroker.repository.ServiceInstanceBindingRepository;
 
 @Service
-public class EcsServiceInstanceBindingService implements ServiceInstanceBindingService {
+public class EcsServiceInstanceBindingService
+	implements ServiceInstanceBindingService {
 
-	@Autowired
-	private EcsService ecs;
+    @Autowired
+    private EcsService ecs;
 
-	@Autowired
-	private ServiceInstanceBindingRepository repository;
+    @Autowired
+    private ServiceInstanceBindingRepository repository;
 
-	public EcsServiceInstanceBindingService()
-			throws EcsManagementClientException, EcsManagementResourceNotFoundException, URISyntaxException {
-		super();
+    public EcsServiceInstanceBindingService()
+	    throws EcsManagementClientException,
+	    EcsManagementResourceNotFoundException, URISyntaxException {
+	super();
+    }
+
+    @Override
+    public CreateServiceInstanceBindingResponse createServiceInstanceBinding(
+	    CreateServiceInstanceBindingRequest request)
+	    throws ServiceInstanceBindingExistsException,
+	    ServiceBrokerException {
+	String instanceId = request.getServiceInstanceId();
+	String bindingId = request.getBindingId();
+	ServiceInstanceBinding binding = new ServiceInstanceBinding(request);
+	Map<String, Object> credentials = new HashMap<>();
+	Map<String, Object> parameters = request.getParameters();
+	credentials.put("accessKey", ecs.prefix(bindingId));
+	credentials.put("bucket", ecs.prefix(instanceId));
+	try {
+	    if (ecs.userExists(bindingId))
+		throw new ServiceInstanceBindingExistsException(instanceId,
+			bindingId);
+
+	    UserSecretKey userSecret = ecs.createUser(bindingId);
+
+	    if (parameters != null) {
+		@SuppressWarnings("unchecked")
+		List<String> permissions = (List<String>) parameters
+			.get("permissions");
+		ecs.addUserToBucket(instanceId, bindingId, permissions);
+	    } else {
+		ecs.addUserToBucket(instanceId, bindingId);
+	    }
+
+	    URL baseUrl = new URL(ecs.getObjectEndpoint());
+	    String userInfo = bindingId + ":" + userSecret.getSecretKey();
+	    String s3Url = baseUrl.getProtocol() + "://" + ecs.prefix(userInfo)
+		    + "@" + baseUrl.getHost() + ":" + baseUrl.getPort() + "/"
+		    + ecs.prefix(instanceId);
+	    credentials.put("secretKey", userSecret.getSecretKey());
+	    credentials.put("endpoint", ecs.getObjectEndpoint());
+	    credentials.put("s3Url", s3Url);
+	    binding.setBindingId(bindingId);
+	    binding.setCredentials(credentials);
+	    repository.save(binding);
+	} catch (IOException | JAXBException | EcsManagementClientException e) {
+	    throw new ServiceBrokerException(e);
 	}
+	return new CreateServiceInstanceAppBindingResponse()
+		.withCredentials(credentials);
+    }
 
-	@Override
-	public CreateServiceInstanceBindingResponse createServiceInstanceBinding(
-			CreateServiceInstanceBindingRequest request)
-					throws ServiceInstanceBindingExistsException, ServiceBrokerException {
-		String instanceId = request.getServiceInstanceId();
-		String bindingId = request.getBindingId();
-		ServiceInstanceBinding binding = new ServiceInstanceBinding(request);
-		Map<String, Object> credentials = new HashMap<>();
-		Map<String, Object> parameters = request.getParameters();
-		credentials.put("accessKey", ecs.prefix(bindingId));
-		credentials.put("bucket", ecs.prefix(instanceId));
-		try {
-			if (ecs.userExists(bindingId))
-				throw new ServiceInstanceBindingExistsException(instanceId, bindingId);
-
-			UserSecretKey userSecret = ecs.createUser(bindingId);
-
-			if (parameters != null) {
-				@SuppressWarnings("unchecked")
-				List<String> permissions = (List<String>) parameters.get("permissions");
-				ecs.addUserToBucket(instanceId, bindingId, permissions);				
-			} else {
-				ecs.addUserToBucket(instanceId, bindingId);
-			}
-
-			URL baseUrl = new URL(ecs.getObjectEndpoint());
-			String userInfo = bindingId + ":" + userSecret.getSecretKey();
-			String s3Url = baseUrl.getProtocol() + "://" + ecs.prefix(userInfo)
-					+ "@" + baseUrl.getHost() + ":" + baseUrl.getPort() + "/"
-					+ ecs.prefix(instanceId);
-			credentials.put("secretKey", userSecret.getSecretKey());
-			credentials.put("endpoint", ecs.getObjectEndpoint());
-			credentials.put("s3Url", s3Url);
-			binding.setBindingId(bindingId);
-			binding.setCredentials(credentials);
-			repository.save(binding);
-		} catch (IOException | JAXBException | EcsManagementClientException e) {
-			e.printStackTrace();
-			throw new ServiceBrokerException(e.getMessage());
-		}
-		return new CreateServiceInstanceAppBindingResponse().withCredentials(credentials);
+    @Override
+    public void deleteServiceInstanceBinding(
+	    DeleteServiceInstanceBindingRequest request)
+	    throws ServiceBrokerException {
+	String bindingId = request.getBindingId();
+	String instanceId = request.getServiceInstanceId();
+	try {
+	    ecs.removeUserFromBucket(instanceId, bindingId);
+	    ecs.deleteUser(bindingId);
+	    repository.delete(bindingId);
+	} catch (Exception e) {
+	    throw new ServiceBrokerException(e);
 	}
-
-	@Override
-	public void deleteServiceInstanceBinding(DeleteServiceInstanceBindingRequest request)
-			throws ServiceBrokerException {
-		String bindingId = request.getBindingId();
-		String instanceId = request.getServiceInstanceId();
-	 	try {
-			ecs.removeUserFromBucket(instanceId, bindingId);
-			ecs.deleteUser(bindingId);
-			repository.delete(bindingId);
-		} catch (Exception e) {
-			throw new ServiceBrokerException(e.getMessage());
-		}
-	}
+    }
 }
