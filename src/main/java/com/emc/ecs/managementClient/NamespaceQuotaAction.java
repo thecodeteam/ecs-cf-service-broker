a53cfda00880217b6a61732bda73260e430980d7
Aaron Spiegel <spiegela@gmail.com>
2016-05-20 23:10:38 -0500

    Extract strings to constants

--- src/main/java/com/emc/ecs/managementClient/NamespaceQuotaAction.java
+++ src/main/java/com/emc/ecs/managementClient/NamespaceQuotaAction.java
@@ -8,29 +8,34 @@ import com.emc.ecs.serviceBroker.EcsManagementClientException;
 
 public class NamespaceQuotaAction {
 
+    private static final String QUOTA = "quota";
+    private static final String OBJECT = "object";
+    private static final String NAMESPACE = "namespace";
+    private static final String NAMESPACES = "namespaces";
+
     private NamespaceQuotaAction() {
     }
 
     public static void create(Connection connection, String namespace,
 	    int limit, int warn) throws EcsManagementClientException {
-	UriBuilder uri = connection.getUriBuilder().segment("object",
-		"namespaces", "namespace", namespace, "quota");
+	UriBuilder uri = connection.getUriBuilder().segment(OBJECT,
+		NAMESPACES, NAMESPACE, namespace, QUOTA);
 	connection.handleRemoteCall("put", uri,
 		new NamespaceQuotaParam(namespace, limit, warn));
     }
 
     public static NamespaceQuotaDetails get(Connection connection,
 	    String namespace) throws EcsManagementClientException {
-	UriBuilder uri = connection.getUriBuilder().segment("object",
-		"namespaces", "namespace", namespace, "quota");
+	UriBuilder uri = connection.getUriBuilder().segment(OBJECT,
+		NAMESPACES, NAMESPACE, namespace, QUOTA);
 	Response response = connection.handleRemoteCall("get", uri, null);
 	return response.readEntity(NamespaceQuotaDetails.class);
     }
 
     public static void delete(Connection connection, String namespace)
 	    throws EcsManagementClientException {
-	UriBuilder uri = connection.getUriBuilder().segment("object",
-		"namespaces", "namespace", namespace, "quota");
+	UriBuilder uri = connection.getUriBuilder().segment(OBJECT,
+		NAMESPACES, NAMESPACE, namespace, QUOTA);
 	connection.handleRemoteCall("delete", uri, null);
     }
 }
