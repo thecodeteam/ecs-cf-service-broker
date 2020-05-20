66848e454eb2a0ae8c32ba5760ca4810a71c4d0f
svistunovaleksandr <Alexander.Svistunov@dell.comm>
2020-05-20 15:49:30 +0300

    test change

--- src/main/java/com/emc/ecs/management/sdk/Constants.java
+++ src/main/java/com/emc/ecs/management/sdk/Constants.java
@@ -24,6 +24,7 @@ public final class Constants {
     public static final String APPLICATION_XML = "application/xml";
     public static final String APPLICATION_JSON = "application/json";
     public static final String TEST_CHANGE = "application/json";
+    public static final String TEST_CHANGE_2 = "application/json";
 
     public static final String GET = "get";
     public static final String PUT = "put";
