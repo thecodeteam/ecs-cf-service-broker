b2fb87d109cdfe6830377dea1633c7c16b5fff41
svistunovaleksandr <Alexander.Svistunov@dell.comm>
2020-05-21 17:37:03 +0300

    test change

--- src/main/java/com/emc/ecs/management/sdk/Constants.java
+++ src/main/java/com/emc/ecs/management/sdk/Constants.java
@@ -29,6 +29,7 @@ public final class Constants {
     public static final String TEST_CHANGE_4 = "application/json";
     public static final String TEST_CHANGE_5 = "application/json";
     public static final String TEST_CHANGE_6 = "application/json";
+    public static final String TEST_CHANGE_7 = "application/json";
 
     public static final String GET = "get";
     public static final String PUT = "put";
