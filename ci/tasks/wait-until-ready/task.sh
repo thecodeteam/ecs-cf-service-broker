85da06f5422631dead00d2365d722853b239c227
svistunovaleksandr <Alexander.Svistunov@dell.comm>
2020-06-08 17:17:50 +0300

    test change

---
+++ ci/tasks/wait-until-ready/task.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export KUBECONFIG=kubeconfig/config
+
+if [[ $DEBUG == true ]]; then
+  set -ex
+else
+  set -e
+fi
+
+FLAGS=()
+
+if [[ "${NAMESPACE}" != "" ]]; then
+  FLAGS=("-n" "$NAMESPACE")
+fi
+
+for i in $(seq 1 $TIMEOUT); do
+
+  ACTUAL_STATUS=$(kubectl get "${KIND}" "${NAME}" "${FLAGS[@]}" -o jsonpath=${CHECK_FIELD})
+
+  echo $ACTUAL_STATUS
+
+  if [[ $ACTUAL_STATUS == DESIRED_STATUS: ]]; then
+    echo "READY"
+    exit 0
+  fi
+
+  sleep 1
+done
+
+echo "Timed out waitng for $KIND $NAME resources to become ready"
+exit 1
