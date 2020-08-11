b0bf20d092219cd0af7594afa3623eb276bb3b93
Rogalchuk, Vyacheslav <vyacheslav.rogalchuk@dell.com>
2020-08-11 12:20:27 +0300

    Specifying response status for custom exceptions

--- src/main/java/com/emc/ecs/servicebroker/EcsManagementClientException.java
+++ src/main/java/com/emc/ecs/servicebroker/EcsManagementClientException.java
@@ -1,5 +1,9 @@
 package com.emc.ecs.servicebroker;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
 public class EcsManagementClientException extends Exception {
     private static final long serialVersionUID = 1L;
 
--- src/main/java/com/emc/ecs/servicebroker/EcsManagementResourceNotFoundException.java
+++ src/main/java/com/emc/ecs/servicebroker/EcsManagementResourceNotFoundException.java
@@ -1,7 +1,11 @@
 package com.emc.ecs.servicebroker;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import java.util.NoSuchElementException;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EcsManagementResourceNotFoundException extends Exception {
     private static final long serialVersionUID = 1L;
 
