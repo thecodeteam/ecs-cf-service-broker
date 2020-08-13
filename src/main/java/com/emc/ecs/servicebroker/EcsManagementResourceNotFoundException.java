9f5cbbc11409be5381f758d6c84c0670a50ae888
Rogalchuk, Vyacheslav <vyacheslav.rogalchuk@dell.com>
2020-08-13 11:45:01 +0300

    Minor fix for exceptions hierarchy

--- src/main/java/com/emc/ecs/servicebroker/EcsManagementResourceNotFoundException.java
+++ src/main/java/com/emc/ecs/servicebroker/EcsManagementResourceNotFoundException.java
@@ -2,7 +2,7 @@ package com.emc.ecs.servicebroker;
 
 import java.util.NoSuchElementException;
 
-public class EcsManagementResourceNotFoundException extends Exception {
+public class EcsManagementResourceNotFoundException extends EcsManagementClientException {
     private static final long serialVersionUID = 1L;
 
     public EcsManagementResourceNotFoundException(String message) {
