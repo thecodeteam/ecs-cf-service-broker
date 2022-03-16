e0cd6c445070bbe8fae9011385ed9a67a3ba7265
Mamedov <Mansur.Mamedov@Dell.com>
2022-03-16 17:28:34 +0300

    bug-fix:deleting bucket

--- src/main/java/com/emc/ecs/servicebroker/service/EcsService.java
+++ src/main/java/com/emc/ecs/servicebroker/service/EcsService.java
@@ -126,6 +126,9 @@ public class EcsService {
     @SuppressWarnings("unchecked")
     Map<String, Object> createBucket(String serviceInstanceId, String bucketName, ServiceDefinitionProxy serviceDefinition,
                                      PlanProxy plan, Map<String, Object> parameters) {
+
+        boolean bucketCreated = false;
+
         try {
             parameters = mergeParameters(broker, serviceDefinition, plan, parameters);
             parameters = validateAndPrepareSearchMetadata(parameters);
@@ -159,6 +162,8 @@ public class EcsService {
                     parameters
             ));
 
+            bucketCreated = true;
+
             if (parameters.containsKey(QUOTA) && parameters.get(QUOTA) != null) {
                 Map<String, Integer> quota = (Map<String, Integer>) parameters.get(QUOTA);
                 logger.info("Applying bucket quota on '{}' in '{}': limit {}, warn {}", prefix(bucketName), namespace, quota.get(QUOTA_LIMIT), quota.get(QUOTA_WARN));
@@ -184,6 +189,12 @@ public class EcsService {
         } catch (Exception e) {
             String errorMessage = String.format("Failed to create bucket '%s': %s", bucketName, e.getMessage());
             logger.error(errorMessage, e);
+
+            if(bucketCreated) {
+                logger.info("Deleting bucket '{}' with service '{}' plan '{}'({}) and params {}", prefix(bucketName), serviceDefinition.getName(), plan.getName(), plan.getId(), parameters);
+                BucketAction.delete(connection, prefix(bucketName), (String) parameters.get(NAMESPACE));
+            }
+
             throw new ServiceBrokerException(errorMessage, e);
         }
         return parameters;
